e570ef722488198024d69e37c521bea6246fb7d9
Juny4541 <1107674541@qq.com>
2018-05-18 15:37:05 +0800

    课设

--- app/src/main/java/com/juny/cashiersystem/business/homepage/menbertab/contract/IMemberContract.java
+++ app/src/main/java/com/juny/cashiersystem/business/homepage/menbertab/contract/IMemberContract.java
@@ -1,6 +1,10 @@
 package com.juny.cashiersystem.business.homepage.menbertab.contract;
 
 import com.juny.cashiersystem.base.IBaseView;
+import com.juny.cashiersystem.realm.bean.CategoryBean;
+import com.juny.cashiersystem.realm.bean.MemberBean;
+
+import io.realm.RealmResults;
 
 /**
  * <br> ClassName: IMemberContract
@@ -11,15 +15,39 @@ import com.juny.cashiersystem.base.IBaseView;
  */
 
 public interface IMemberContract {
-    interface IView extends IBaseView {
 
+    interface IView extends IBaseView {
+        /**
+         * <br> Description:
+         * 数据
+         * <br> Author: chenrunfang
+         * <br> Date: 2018/4/8 17:23
+         */
+        void showMemberData(RealmResults<MemberBean> MemberList);
     }
 
-    interface Ipresenter {
-
+    interface IPresenter {
+        /**
+         * <br> Description: 获取分类数据
+         * <br> Author: chenrunfang
+         * <br> Date: 2018/4/8 17:23
+         */
+        void getMemberData();
     }
 
     interface IModel {
+        /**
+         * <br> Description: 查询分类数据
+         * <br> Author: chenrunfang
+         * <br> Date: 2018/5/10 15:16
+         */
+        RealmResults<MemberBean> searchMemberData();
 
+        /**
+         * <br> Description: 添加分类
+         * <br> Author: chenrunfang
+         * <br> Date: 2018/5/10 15:16
+         */
+        void addMember(CategoryBean categoryBean);
     }
 }
